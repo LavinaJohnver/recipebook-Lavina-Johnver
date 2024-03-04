7b17d6cc92d170a18b86bd361623ead8d8c90d9c
JohnverLavina <johnver.lavina@student.ateneo.edu>
2024-03-04 20:27:45 +0800

    add: admin panel

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class RecipeInLine(admin.StackedInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeInLine,]
+
+class RecipeIngredientInline(admin.StackedInline):  
+    model = RecipeIngredient
+
+class IngredientAdmin(admin.ModelAdmin):    
+    model = Ingredient
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+
+
