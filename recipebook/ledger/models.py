b4956ddcfec2e1a0cf9c2a9c8af83517a60d300b
JohnverLavina <johnver.lavina@student.ateneo.edu>
2024-03-04 20:12:33 +0800

    add: string representations and absolute url functions

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -3,9 +3,21 @@ from django.urls import reverse
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[self.pk])
     
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient_detail', args=[self.pk])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
