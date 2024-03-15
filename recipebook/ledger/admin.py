9f63766fc334a3d6c96cee5e6e35ca663b58c648
JohnverLavina <johnver.lavina@student.ateneo.edu>
2024-03-15 15:27:34 +0800

    modified: ledger admin (added user function), recipebook settings (added urls and email backend)

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,6 +1,9 @@
 # Register your models here.
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
 
 class RecipeInLine(admin.StackedInline):
     model = RecipeIngredient
@@ -18,4 +21,17 @@ class IngredientAdmin(admin.ModelAdmin):
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [
+        ProfileInline,
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
