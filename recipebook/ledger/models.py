b74d63681ee370813d9982577463ef27f63443a0
JohnverLavina <johnver.lavina@student.ateneo.edu>
2024-03-15 15:06:03 +0800

    modified: ledger models and added date fiels

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.urls import reverse
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -31,4 +36,17 @@ class RecipeIngredient(models.Model):
         on_delete = models.CASCADE,
         related_name = 'recipe',
     )  
+
+    def __str__(self):
+                return f'{self.quantity} {self.ingredient.name} in {self.recipe.name}'
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(255, "Bio must be at least 255 characters long.")
+        ]
+    )
+
 
